ccb176c2b728881b72cf4cfbaefe6ac04b2c5a6e
Jan Koudijs <theappgineer@gmail.com>
2022-01-11 18:58:27 +0100

    Use rem service for container (re)creation, use docker service for container (re)start

--- recipes-rem/rem/files/rem.sh
+++ recipes-rem/rem/files/rem.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-docker container inspect roon-extension-manager > /dev/null 2>&1
+STATUS=$(docker container inspect roon-extension-manager --format='{{.State.Status}}')
 
 if [ $? -eq 0 ]; then
-    docker start roon-extension-manager
-    docker attach roon-extension-manager
+    if [ "$STATUS" = "exited" ]; then
+        docker start roon-extension-manager
+    fi
 else
     DOCKER_GID=$(stat -c '%g' /var/run/docker.sock 2> /dev/null)
     TZ=$(timedatectl | sed -n 's/^\s*Time zone: \(.*\) (.*/\1/p')
 
-    docker run --network host --name roon-extension-manager --group-add ${DOCKER_GID} -v /home/rem:/home/node/.rem/ -v /var/run/docker.sock:/var/run/docker.sock -e "TZ=${TZ}" --log-driver journald theappgineer/roon-extension-manager:v1.x
+    docker run -d --network host --restart unless-stopped --name roon-extension-manager --group-add ${DOCKER_GID} -v /home/rem:/home/node/.rem/ -v /var/run/docker.sock:/var/run/docker.sock -e "TZ=${TZ}" --log-driver journald theappgineer/roon-extension-manager:v1.x
 
     if [ $? -ne 0 ]; then
         exit 1
     fi
 fi
+
+docker attach roon-extension-manager
 
 EXIT_CODE=$(docker container inspect roon-extension-manager --format='{{.State.ExitCode}}')
 
